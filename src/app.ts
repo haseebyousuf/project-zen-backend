d101e3a623a68ab3e33e0bb852b9d680537c87e0
Haseeb <haseebyousuff@gmail.com>
2025-04-06 19:01:28 +0530

    feat(app): add authentication routes and middleware setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import healthCheckRouter from "./routes/health-check.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
+//MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ROUTES
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
 export default app;
